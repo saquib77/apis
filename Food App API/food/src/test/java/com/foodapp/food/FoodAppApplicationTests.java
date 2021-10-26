104f9df90875249cbd1a24cb5c6e932974b3148e
saquib77 <chand567khan@gmail.com>
2021-10-26 10:53:39 +0530

    food app api

---
+++ Food App API/food/src/test/java/com/foodapp/food/FoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapp.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
